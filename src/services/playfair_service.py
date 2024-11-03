3ac8e1e540f0f11297d4b3c4c41ad4be748fd867
Kevin Kfoury <166258794+SeeKraken1@users.noreply.github.com>
2024-11-03 23:34:29 +0200

    Update playfair_service.py

--- src/services/playfair_service.py
+++ src/services/playfair_service.py
@@ -3,7 +3,6 @@ import string
 alphabet = 'ABCDEFGHIKLMNOPQRSTUVWXYZ'  # I and J are typically combined for Playfair
 
 def create_playfair_key_matrix(key):
-    # Remove duplicates and prepare the key matrix
     key = ''.join(sorted(set(key), key=key.index)).upper().replace('J', 'I')
     key_matrix = [c for c in key if c in alphabet]
     for letter in alphabet:
@@ -12,7 +11,6 @@ def create_playfair_key_matrix(key):
     return [key_matrix[i:i + 5] for i in range(0, 25, 5)]
 
 def find_position(char, key_matrix):
-    # Find position of a character in the key matrix
     for row in range(5):
         for col in range(5):
             if key_matrix[row][col] == char:
@@ -24,33 +22,40 @@ def playfair_encryption(plaintext, key):
     plaintext = plaintext.upper().replace('J', 'I')
     plaintext_pairs = []
     
-    # Prepare plaintext pairs
     i = 0
     while i < len(plaintext):
         char1 = plaintext[i]
-        char2 = plaintext[i + 1] if i + 1 < len(plaintext) else 'X'
+        if not char1.isalpha():
+            plaintext_pairs.append((char1,))
+            i += 1
+            continue
+
+        char2 = plaintext[i + 1] if i + 1 < len(plaintext) and plaintext[i + 1].isalpha() else 'X'
         
-        # Handle duplicate letters in pair
         if char1 == char2:
             plaintext_pairs.append((char1, 'X'))
             i += 1
         else:
             plaintext_pairs.append((char1, char2))
             i += 2
-    
-    # Encrypt pairs
+
     encrypted_text = []
-    for char1, char2 in plaintext_pairs:
+    for pair in plaintext_pairs:
+        if len(pair) == 1:
+            encrypted_text.append(pair[0])
+            continue
+        
+        char1, char2 = pair
         row1, col1 = find_position(char1, key_matrix)
         row2, col2 = find_position(char2, key_matrix)
         
-        if row1 == row2:  # Same row
+        if row1 == row2:
             encrypted_text.append(key_matrix[row1][(col1 + 1) % 5])
             encrypted_text.append(key_matrix[row2][(col2 + 1) % 5])
-        elif col1 == col2:  # Same column
+        elif col1 == col2:
             encrypted_text.append(key_matrix[(row1 + 1) % 5][col1])
             encrypted_text.append(key_matrix[(row2 + 1) % 5][col2])
-        else:  # Rectangle swap
+        else:
             encrypted_text.append(key_matrix[row1][col2])
             encrypted_text.append(key_matrix[row2][col1])
     
@@ -61,21 +66,38 @@ def playfair_decryption(ciphertext, key):
     ciphertext = ciphertext.upper().replace('J', 'I')
     decrypted_text = []
 
-    for i in range(0, len(ciphertext), 2):
+    i = 0
+    while i < len(ciphertext):
         char1 = ciphertext[i]
-        char2 = ciphertext[i + 1]
+        if not char1.isalpha():
+            decrypted_text.append(char1)
+            i += 1
+            continue
+        
+        char2 = ciphertext[i + 1] if i + 1 < len(ciphertext) and ciphertext[i + 1].isalpha() else 'X'
+        
         row1, col1 = find_position(char1, key_matrix)
         row2, col2 = find_position(char2, key_matrix)
-        
-        if row1 == row2:  # Same row
+
+        if row1 == row2:
             decrypted_text.append(key_matrix[row1][(col1 - 1) % 5])
             decrypted_text.append(key_matrix[row2][(col2 - 1) % 5])
-        elif col1 == col2:  # Same column
+        elif col1 == col2:
             decrypted_text.append(key_matrix[(row1 - 1) % 5][col1])
             decrypted_text.append(key_matrix[(row2 - 1) % 5][col2])
-        else:  # Rectangle swap
+        else:
             decrypted_text.append(key_matrix[row1][col2])
             decrypted_text.append(key_matrix[row2][col1])
-    
-    # Remove extra 'X's if they were added during encryption
-    return ''.join(decrypted_text).replace('X', '')
+        
+        i += 2
+
+    # Remove padding 'X' characters
+    cleaned_text = []
+    for i, char in enumerate(decrypted_text):
+        if i < len(decrypted_text) - 1 and char == 'X' and decrypted_text[i - 1] == decrypted_text[i + 1]:
+            continue  # Skip padding 'X'
+        if i == len(decrypted_text) - 1 and char == 'X':
+            continue  # Skip trailing 'X'
+        cleaned_text.append(char)
+
+    return ''.join(cleaned_text)
