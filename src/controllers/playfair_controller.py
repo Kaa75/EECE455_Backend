97db7719c93428cb97d533b0ed26e9afcaa3731f
Kevin Kfoury <166258794+SeeKraken1@users.noreply.github.com>
2024-11-04 23:49:28 +0200

    Update playfair_controller.py

--- src/controllers/playfair_controller.py
+++ src/controllers/playfair_controller.py
@@ -6,36 +6,41 @@ def is_valid_key(key):
     # Check if key is a string and contains only alphabetic characters
     return isinstance(key, str) and key.isalpha()
 
-def encrypt():
+def encrypt(request):
   '''Encrypts plaintext using Playfair cipher after getting the necessary input parameters from the user.
     parameters:
     text = plaintext obtained from the user
     key = key obtained from the user 
     '''
-    text = request.args.get('text', '')
-    key = request.args.get('key', '')
+    text = request.get('inputText', '')
+    key = request.get('keyString', '')
+    cipher = request.get('cipher', '').lower()
+    if cipher != 'playfair':
+        return jsonify({'error': 'Invalid cipher type. Use "cipher=playfair".'}), 400
     if not is_valid_key(key):
      return jsonify({"error": "Invalid key. Key must be a string of alphabetic characters only."}), 400
 
     # Call the Playfair encryption function
     encrypted_text = playfair_encryption(text, key)
-    return jsonify({"ciphertext": encrypted_text})
+    return jsonify({"encrypted_text": encrypted_text})
 
-def decrypt():
+def decrypt(request):
     '''Decrypts ciphertext using Playfair cipher after getting the necessary input parameters from the user.
     parameters:
     text = ciphertext obtained from the user
     key = key obtained from the user 
     '''
-    data = request.get_json()
-    ciphertext = data.get('ciphertext', '')
-    key = data.get('key', '')
+    text = request.get('inputText', '')
+    key = request.get('keyString', '')
+    cipher = request.get('cipher', '').lower()
 
+    if cipher != 'playfair':
+        return jsonify({'error': 'Invalid cipher type. Use "cipher=playfair".'}), 400
     # Validate the key
     if not is_valid_key(key):
         return jsonify({"error": "Invalid key. Key must be a string of alphabetic characters only."}), 400
 
     # Call the Playfair decryption function
-    decrypted_text = playfair_decryption(ciphertext, key)
-    return jsonify({"plaintext": decrypted_text})
+    decrypted_text = playfair_decryption(text, key)
+    return jsonify({'decrypted_text': decrypted_text})
   
