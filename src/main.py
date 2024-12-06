dba6795d7cf217cd7af992197f5f780c115a71a4
hadi-mubasher <h.mubasher99@outlook.com>
2024-12-07 00:19:10 +0200

    removed code in main

--- src/main.py
+++ src/main.py
@@ -7,18 +7,3 @@ from controllers import (
     mono_alphabetic_controller,
     euclid_controller,
 )
-
-app = Flask(__name__)
-
-
-# Register routes
-app.add_url_rule('/encrypt/affine', view_func=affine_controller.encrypt_route, methods=['GET'])
-app.add_url_rule('/decrypt/affine', view_func=affine_controller.decrypt_route, methods=['GET'])
-app.add_url_rule('/crack/affine', view_func=affine_controller.crack_route, methods=['GET'])
-
-
-
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
