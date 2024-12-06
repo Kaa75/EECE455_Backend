5402debd715a03f1dc01f504a52590323fbed833
hadi-mubasher <h.mubasher99@outlook.com>
2024-12-07 00:18:48 +0200

    removed code in main

--- src/main.py
+++ src/main.py
@@ -8,15 +8,3 @@ from controllers import (
     euclid_controller,
 )
 
-app = Flask(__name__)
-
-
-# Register routes
-app.add_url_rule('/encrypt/hill', view_func=hill_controller.encrypt_route, methods=['GET'])
-app.add_url_rule('/decrypt/hill', view_func=hill_controller.decrypt_route, methods=['GET'])
-
-
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
